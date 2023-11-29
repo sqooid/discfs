3f21f47c6ab883441695f5c354721b21dcaf43f9
sqooid <sqooiddev@gmail.com>
2023-11-29 16:20:06 +1100

    init

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+  outputs = { self, nixpkgs, ... }:
+    let
+      forSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        # "aarch64-darwin"
+      ];
+      pkgsFor = forSystem (system:
+        import nixpkgs { inherit system; }
+      );
+    in
+    {
+      packages = forSystem
+        (system:
+          let
+            pkgs = pkgsFor."${system}";
+          in
+          {
+            # app = pkgs.callPackage ./path { };
+          }
+        );
+      devShells = forSystem
+        (system:
+          let
+            pkgs = pkgsFor."${system}";
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [ cargo ];
+            };
+          }
+        );
+    };
+}

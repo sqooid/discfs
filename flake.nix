e84cfd69bac61eb5c2bf3c6967d87293b10fa84f
sqooid <sqooiddev@gmail.com>
2023-12-04 16:02:40 +1100

    working flake build

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, crane, ... }:
     let
       forSystem = nixpkgs.lib.genAttrs [
         "x86_64-linux"
-        # "aarch64-darwin"
+        "aarch64-linux"
       ];
       pkgsFor = forSystem (system:
         import nixpkgs { inherit system; }
@@ -17,9 +19,24 @@
         (system:
           let
             pkgs = pkgsFor."${system}";
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+            craneLib = crane.lib.${system};
+            sqlFilter = path: _type: null != builtins.match ".*\.sql.*" path;
+            sqlOrCargo = path: type: (sqlFilter path type) || (craneLib.filterCargoSources path type);
           in
           {
-            # app = pkgs.callPackage ./path { };
+            discfs = craneLib.buildPackage {
+
+              src = pkgs.lib.cleanSourceWith {
+                src = craneLib.path ./.; # The original, unfiltered source
+                filter = sqlOrCargo;
+              };
+
+              doCheck = false;
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+              buildInputs = with pkgs; [ openssl.dev sqlite fuse3 ];
+            };
+            default = self.packages.${system}.discfs;
           }
         );
       devShells = forSystem
